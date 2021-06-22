61aaeeef9c61da726bf3043b1160db991f496f44
eric-quattropani <eric.quattropani@gmail.com>
2021-06-22 17:47:08 +1000

    Final commit
    Updated all of script.js as well as the readme file.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Techdegree-Project-3-Interactive-Form
+ This is my attempt at Project 3: Interactive Form.
+
+ I honestly found it pretty challenging, and it took me quite a number of hours to get through it. But I'm glad I've come out the other side. 
  
+ Lots to learn from!
--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-// A bunch of variables that will be storing all the form inputs //
-
+// 3. The "Name" field.
 const nameElement = document.querySelector("#name");
 nameElement.focus();
 
 const jobroleElement = document.querySelector("#title");
 const otherjobroleElement = document.querySelector("#other-job-role");
 otherjobroleElement.style.display = "none";
-//const allJobs = document.querySelectorAll("#job");
 
-
+//4. "Job Role" section
 //conditional that displays and hides the Other Job Role text field when someone selects Other in the Job Role dropdown menu.
 jobroleElement.addEventListener("change", (e) => {
     if (e.target.value === "other") {
@@ -18,6 +16,7 @@ jobroleElement.addEventListener("change", (e) => {
     }
 });
 
+//5. "T-Shirt info" section
 const designElement = document.querySelector("#design");
 const colorElement = document.querySelector("#color");
 const colorelementOptions = colorElement.children;
@@ -52,4 +51,166 @@ designElement.addEventListener("change", (e) => {
 
 });
 
+//6. "Register for Activities" section
+let registerActivities = document.querySelector("#activities");
+let total = document.querySelector("#activities-cost");
+let totalCost = 0;
 
+registerActivities.addEventListener("change", (e) => {
+    const choice = e.target;
+    let choiceCost = choice.getAttribute("data-cost");
+    choiceCost = Number(choiceCost);
+    if (choice.checked) {
+        totalCost += choiceCost;
+    } else {
+        totalCost -= choiceCost;
+    }
+    total.innerHTML = `Total: $${totalCost}`;
+});
+
+
+
+
+//7. "Payment Info" section
+const paymentMenu = document.querySelector("#payment");
+const creditCard = document.getElementById('credit-card');
+const paypal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
+
+paypal.style.display = "none";
+bitcoin.style.display = "none";
+
+paymentMenu[1].selected = true; //--> Automatically selecting the Credit Card. 
+
+paymentMenu.addEventListener ('change', (e) => {
+    const payDecision = e.target.value;
+    if (payDecision === "paypal") {
+        creditCard.style.display = "none";
+        paypal.style.display = "block";
+        bitcoin.style.display = "none";
+    } else if (payDecision === "bitcoin"){
+        creditCard.style.display = "none";
+        bitcoin.style.display = "block";
+        paypal.style.display= "none";
+    } else {
+        creditCard.style.display = "block";
+        bitcoin.style.display = "none";
+        paypal.style.display= "none";
+    }
+});
+
+//8. Form Validation
+const emailAddress = document.querySelector("#email");
+const creditCardNumber = document.getElementById('cc-num');
+const zipCode = document.getElementById('zip');
+const cvv = document.getElementById('cvv');
+const form = document.querySelector("form");
+const activitiesBox = document.getElementById('activities-box');
+const paymentMethod = document.getElementById('payment');
+
+function validForm(element) {
+    element.parentElement.classList.add("valid");
+    element.parentElement.classList.remove("not-valid");
+    element.parentElement.lastElementChild.style.display = "none";
+};
+
+function invalidForm(element) {
+    element.parentElement.classList.add("not-valid");
+    element.parentElement.classList.remove("valid");
+    element.parentElement.lastElementChild.style.display = "block";
+};
+
+//validating the name field
+function validName() {
+    const nameTest = /.*\S.*/.test(nameElement.value); //-> took the regex code from https://regexr.com/ 
+    if (nameTest == true) {
+        validForm(nameElement);
+    } else {
+        invalidForm(nameElement);
+    } return nameTest;    
+};
+
+//validating the email field
+function validEmail() {
+    const emailTest = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi.test(emailAddress.value); //-> took the regex code from https://regexr.com/
+    if (emailTest == true) {
+        validForm(emailAddress);
+    } else {
+        invalidForm(emailAddress);
+    } return emailTest;
+};
+
+//validating the activity section
+function validActivity() {
+    let activityTest = totalCost > 0;
+    if (activityTest) {
+        validForm(activitiesBox);
+    } else {
+        invalidForm(activitiesBox);
+    } return activityTest;
+};
+
+// validating the credit card field
+function validCard() {
+    const cardTest = /^[0-9]{13,16}$/.test(creditCardNumber.value);
+    if (cardTest == true) {
+        validForm(creditCardNumber)
+    } else {
+        invalidForm(creditCardNumber)
+    } return cardTest;
+};
+
+// validating the zip code field
+function validZipCode() {
+    const zipcodeTest = /^[0-9]{5}$/.test(zipCode.value);
+    if (zipcodeTest == true) {
+        validForm(zipCode)
+    } else {
+        invalidForm(zipCode)
+    } return zipcodeTest;
+};
+
+// validating the cvv field
+function validCvv() {
+    const cvvTest = /^[0-9]{3}$/.test(cvv.value);
+    if (cvvTest == true) {
+        validForm(cvv)
+    } else {
+        invalidForm(cvv)
+    } return cvvTest;
+};
+
+
+form.addEventListener('submit', (e) => {
+    if (!validName()) {
+        e.preventDefault();
+    } if (!validEmail()) {
+        e.preventDefault();
+    } if (!validActivity()) {
+        e.preventDefault();
+    }
+    if (paymentMenu.value === 'credit-card') {
+        if (!validCard()) {
+            e.preventDefault();
+        }
+
+        if (!validCvv()) {
+            e.preventDefault();
+        }
+
+        if (!validZipCode()) {
+            e.preventDefault();
+        }
+    }
+});
+
+//9. Accessibility
+const activitiesAccessibility = document.querySelectorAll('input[type=checkbox]');
+for (let i = 0; i < activitiesAccessibility.length; i += 1) {
+    activitiesAccessibility[i].addEventListener("focus", e => {
+        activitiesAccessibility[i].parentElement.classList.add("focus");
+    });
+    activitiesAccessibility[i].addEventListener("blur", e => {
+        activitiesAccessibility[i].parentElement.classList.remove("focus");
+    });
+  };
